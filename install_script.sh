a660bf4f4f80db82760c07c81205a6bab68f19e0
elkiss-centos <jominhyuk87@gmail.com>
2016-03-23 09:47:53 -0400

    Push Install Script

---
+++ install_script.sh
@@ -0,0 +1,18 @@
+udo yum install gcc gcc-c++ make -y
+
+sudo yum install ruby ruby-devel ruby-irb ruby-rdoc ruby-ri
+
+\curl -L https://get.rvm.io | bash
+
+export PATH=/usr/local/bin:/usr/local/sbin:/usr/bin:/bin:/sbin:/usr/sbin
+[[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm" # Load RVM function
+
+rvm install 1.9.3
+rvm --default 1.9.3
+
+sudo gem update
+sudo gem update --system
+sudo gem install rails
+
+sudo yum install sqlite-devel
+sudo gem install sqlite3-ruby
